96d5beccf358f4e4037b2d237e6a3ffd72d523c9
ilia24 <iliademertchian24@gmail.com>
2017-03-31 20:11:24 -0400

    Add automatic book ID generation upon Book.create

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
 class Book
   @@on_shelf = []
   @@on_loan = []
+  @@bookid = 0
 
   attr_accessor :due_date
 
@@ -10,9 +11,12 @@ class Book
     @name = name
     @author = author
     @isbn = isbn
+    @id = @@bookid += 1
   end
 
   def self.create(name, author, isbn)
     new_book = new(name, author, isbn)
+    @@on_shelf << new_book
+    new_book
   end
 end
