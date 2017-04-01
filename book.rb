5aef46cbda182fbffd3bbe80e831e708a801ce19
ilia24 <iliademertchian24@gmail.com>
2017-03-31 20:06:00 -0400

    Add initialize/create methods

--- book.rb
+++ book.rb
@@ -3,4 +3,16 @@ class Book
   @@on_loan = []
 
   attr_accessor :due_date
+
+
+
+  def initialize(name, author, isbn)
+    @name = name
+    @author = author
+    @isbn = isbn
+  end
+
+  def self.create(name, author, isbn)
+    new_book = new(name, author, isbn)
+  end
 end
