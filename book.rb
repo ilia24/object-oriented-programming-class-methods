7ab4d5da467204b55a61852d8743a9dabb6a833d
ilia24 <iliademertchian24@gmail.com>
2017-03-31 21:26:29 -0400

    Add proper output for borrow and shelf check methods

--- book.rb
+++ book.rb
@@ -4,6 +4,7 @@ class Book
   @@bookid = 0
 
   attr_accessor :due_date
+  attr_reader :name, :author
 
   def initialize(name, author, isbn)
     @name = name
@@ -13,16 +14,41 @@ class Book
   end
 
   def self.create(name, author, isbn)
-    new_book = new(name, author, isbn)
+    new_book = Book.new(name, author, isbn)
     @@on_shelf << new_book
     new_book
   end
 
   def self.available
-    @@on_shelf
+    total_books = 0
+    puts "Current books available for borrowing:"
+    @@on_shelf.each do |book|
+      total_books += 1
+      puts "- #{book.name} by #{book.author}"
+    end
+    puts "There are a total of #{total_books} books available"
   end
 
   def self.borrowed
-    @@on_loan
+    @@on_loan.each do |book|
+      puts "- #{book.name} by #{book.author}"
+    end
   end
+
+  def self.browse
+    @@on_shelf.sample
+  end
+
+  # def lent_out?
+  #   if @@on_shelf.include?(self)
+  #     puts "#{self[0]} is currently lent out"
+  #   end
+  # end
 end
+
+
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+jimstale = Book.create("The tales of Jim", "Jim", "2342423")
+jimstale = Book.create("The tales of Jim", "Jim", "2342423")
