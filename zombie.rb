a56767f1217c16379e2b8ef098f0d458bd3965c3
ilia24 <iliademertchian24@gmail.com>
2017-04-01 14:57:59 -0400

    Add plague return, and plague increase class methods

--- zombie.rb
+++ zombie.rb
@@ -5,4 +5,20 @@ class Zombie
   @@max_strength = 8
   @@default_speed= 1
   @@default_strength = 3
+
+#CLASS METHODS
+#This method returns the horde value
+  def self.all
+    @@plague_level
+  end
+
+#increases plague by a randum num between 0-2
+  def self.increase_plague_level
+    @@plague_level += rand(3)
+  end
+
+
+
+#INSTANCE METHODS
+
 end
